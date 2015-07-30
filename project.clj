60516ccc7d4a167db8e23fa0590115fdb9742002
Steve Ashton <nothsaevets@gmail.com>
2015-07-29 22:02:06 -0400

    Removed nrepl dependency

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject shellac "0.1.0-SNAPSHOT"
+(defproject shellac/shellac "0.1.0-SNAPSHOT"
   :description "Shellac is a Clojure command-line enhancer. It provides the capability
    to write commands in Clojure, and run them from the command line without incurring
    the cost of a JVM/Clojure startup each time."
@@ -6,7 +6,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [environ "1.0.0"]
-                 [org.clojure/tools.nrepl "0.2.10"]]
+                 [environ "1.0.0"]]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
